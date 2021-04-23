527303caf1c181a79f25283dc59e6e3d07ee57e3
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2021-04-23 17:06:11 +0100

    Compiling with gdb debug symbols

--- Tests/small-tests/build.sh
+++ Tests/small-tests/build.sh
@@ -4,8 +4,8 @@ riscv64-unknown-elf-gcc -o init.o -c init.s
 for TEST in toy dual hpm memshim
 do
 riscv64-unknown-elf-gcc -o $TEST.o -c $TEST.c
-riscv64-unknown-elf-gcc -nostdlib -mcmodel=medany -Tlink.ld -o $TEST $TEST.o init.o
-riscv64-unknown-elf-gcc -nostdlib -mcmodel=medany -Thwlink.ld -o $TEST.hw $TEST.o init.o
+riscv64-unknown-elf-gcc -g -nostdlib -mcmodel=medany -Tlink.ld -o $TEST $TEST.o init.o
+riscv64-unknown-elf-gcc -g -nostdlib -mcmodel=medany -Thwlink.ld -o $TEST.hw $TEST.o init.o
 riscv64-unknown-elf-objdump -D $TEST > $TEST.dump
 ../elf_to_hex/elf_to_hex $TEST $TEST.hex
 done
