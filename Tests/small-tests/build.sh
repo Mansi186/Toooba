0a0b9b9005600f2366dc64d18230fc416bb5e9ee
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2021-04-23 17:04:16 +0100

    Building for running on hardware as well.

--- Tests/small-tests/build.sh
+++ Tests/small-tests/build.sh
@@ -5,6 +5,7 @@ for TEST in toy dual hpm memshim
 do
 riscv64-unknown-elf-gcc -o $TEST.o -c $TEST.c
 riscv64-unknown-elf-gcc -nostdlib -mcmodel=medany -Tlink.ld -o $TEST $TEST.o init.o
+riscv64-unknown-elf-gcc -nostdlib -mcmodel=medany -Thwlink.ld -o $TEST.hw $TEST.o init.o
 riscv64-unknown-elf-objdump -D $TEST > $TEST.dump
 ../elf_to_hex/elf_to_hex $TEST $TEST.hex
 done
