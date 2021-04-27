68ccf33300eeffb36d6b2b149bbedeea8d1bca3b
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2021-04-27 12:15:58 +0100

    Make sure debug symbols are generated

--- Tests/small-tests/build.sh
+++ Tests/small-tests/build.sh
@@ -3,7 +3,7 @@
 riscv64-unknown-elf-gcc -o init.o -c init.s
 for TEST in toy dual hpm memshim loop
 do
-riscv64-unknown-elf-gcc -o $TEST.o -c $TEST.c
+riscv64-unknown-elf-gcc -g -o $TEST.o -c $TEST.c
 riscv64-unknown-elf-gcc -g -nostdlib -mcmodel=medany -Tlink.ld -o $TEST $TEST.o init.o
 riscv64-unknown-elf-gcc -g -nostdlib -mcmodel=medany -Thwlink.ld -o $TEST.hw $TEST.o init.o
 riscv64-unknown-elf-objdump -D $TEST > $TEST.dump
