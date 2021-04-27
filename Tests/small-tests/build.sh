dd5d4f5c173363b683e830d6d1eeb975ffc7102b
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2021-04-27 11:36:24 +0100

    Added loop benchmark and cleaned up gitignore

--- Tests/small-tests/build.sh
+++ Tests/small-tests/build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh -xe
 
 riscv64-unknown-elf-gcc -o init.o -c init.s
-for TEST in toy dual hpm memshim
+for TEST in toy dual hpm memshim loop
 do
 riscv64-unknown-elf-gcc -o $TEST.o -c $TEST.c
 riscv64-unknown-elf-gcc -g -nostdlib -mcmodel=medany -Tlink.ld -o $TEST $TEST.o init.o
 riscv64-unknown-elf-gcc -g -nostdlib -mcmodel=medany -Thwlink.ld -o $TEST.hw $TEST.o init.o
 riscv64-unknown-elf-objdump -D $TEST > $TEST.dump
+riscv64-unknown-elf-objdump -D $TEST.hw > $TEST.dumphw
 ../elf_to_hex/elf_to_hex $TEST $TEST.hex
 done
---
+++ Tests/small-tests/loop.c
@@ -0,0 +1,12 @@
+// See LICENSE file for the license details of MIT
+// Copyright 2020 Marno van der Maas
+
+void someFunction(void) {
+    return;
+}
+
+long main(char * output) {
+    while(1) {
+        someFunction();
+    }
+}
