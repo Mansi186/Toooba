50028980628d8d8743c8d68073a73a9d3c279779
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2021-05-19 15:13:15 +0100

    Added hello minimal benchmark

--- Tests/small-tests/build.sh
+++ Tests/small-tests/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -xe
 
 riscv64-unknown-elf-gcc -o init.o -c init.s
-for TEST in toy dual hpm memshim loop memshimwrite
+for TEST in toy dual hpm memshim loop memshimwrite hello
 do
 riscv64-unknown-elf-gcc -g -o $TEST.o -c $TEST.c
 riscv64-unknown-elf-gcc -g -nostdlib -mcmodel=medany -Tlink.ld -o $TEST $TEST.o init.o
