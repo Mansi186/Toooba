0ee3d8af32b21fbe4516c5541675e44f5e3b9987
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2021-05-27 16:16:46 +0100

    Bump praesidio-hardware and made hello benchmark default test

--- builds/RV64ACDFIMSUxCHERI_Toooba_verilator/runsmalltest.sh
+++ builds/RV64ACDFIMSUxCHERI_Toooba_verilator/runsmalltest.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -xe
 
-cp ../../Tests/small-tests/memshimwrite.hex Mem.hex
+cp ../../Tests/small-tests/hello.hex Mem.hex
 cp ../../Tests/small-tests/symbol_table.txt .
 ./exe_HW_sim +tohost
