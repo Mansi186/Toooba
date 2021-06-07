f450bdf78180a398427bd4ae15e9e54c6245501f
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2021-06-07 17:01:32 +0100

    Using rdcycle instead of rdtime for read benchmark

--- Tests/small-tests/memshimread.c
+++ Tests/small-tests/memshimread.c
@@ -2,18 +2,18 @@ int main(void) {
     int ret;
     //Loop that counts how long it takes for doing loads from DRAM 
     asm volatile (
-        "li t0, 200;"
+        "li t0, 10;"
         "auipc t2, 0x100;"
         "srli t2, t2, 2;"
         "slli t2, t2, 2;"
         "addi t2, t2, 0x7F0;"
-        "rdtime t1;"
+        "rdcycle t1;"
         "loop: addi t0, t0, -1;"
         "slli t3, t0, 16;"
         "add t4, t2, t3;"
         "lw t4, 0(t4);"
         "bnez t0, loop;"
-        "rdtime t0;"
+        "rdcycle t0;"
         "sub %0, t0, t1"
         : "=r"(ret) //output
         : //input
