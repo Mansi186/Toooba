068c7c5c4e73e90d03a282fe7378ee6e219a8e38
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2021-06-08 15:02:29 +0100

    Switch to no secure core praesidio-hardware and made loads dependent on previous one

--- Tests/small-tests/memshimread.c
+++ Tests/small-tests/memshimread.c
@@ -2,14 +2,16 @@ int main(void) {
     int ret;
     //Loop that counts how long it takes for doing loads from DRAM 
     asm volatile (
-        "li t0, 10;"
+        "li t0, 1000;"
         "auipc t2, 0x100;"
         "srli t2, t2, 2;"
         "slli t2, t2, 2;"
         "addi t2, t2, 0x7F0;"
+        "addi t4, zero, 0;"
         "rdcycle t1;"
         "loop: addi t0, t0, -1;"
         "slli t3, t0, 16;"
+        "add t3, t3, t4;"
         "add t4, t2, t3;"
         "lw t4, 0(t4);"
         "bnez t0, loop;"
